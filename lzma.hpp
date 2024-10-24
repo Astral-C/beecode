a1dd685a820eead8c7e8e2b13da654842a255043
Astral-C <astral-c@users.noreply.github.com>
2024-10-24 15:06:09 -0400

    more work on lzma

--- lzma.hpp
+++ lzma.hpp
@@ -18,6 +18,57 @@ T swap(T v){
     return swapped;
 }
 
+struct DecoderOutput {
+    uint8_t* buffer { nullptr };
+    uint8_t* position { nullptr };
+    std::size_t size { 0 };
+    uint16_t runningTotal { 0 };
+
+    uint8_t*& stream { nullptr };
+    uint32_t maxStreamSize { 0 };
+
+    void operator+=(uint8_t b) { // put
+        runningTotal++;
+        *position = b; position++;
+        if(position == buffer + size){
+            position = buffer;
+        }
+        *stream = b;
+        stream++;
+    }
+
+    uint8_t operator[](std::size_t dist) const { // get
+        return dist <= static_cast<std::size_t>(position - buffer) ? *(position - dist) : *(position + (size - dist));
+    }
+
+    void PreformCopy(std::size_t dist, uint16_t len){
+        while(len >  0){
+            (*this) += (*this)[dist];
+            len--;
+        }
+    }
+
+    bool ValidateDist(std::size_t dist) {
+        return dist <= static_cast<std::size_t>(position - buffer) || position == (buffer + size);
+    }
+
+    bool IsEmpty() {
+        return pos == buffer || position != (buffer + size);
+    }
+
+    DecoderOutput(std::size_t dictSize, uint8_t*& strm, uint32_t streamMax){
+        buffer = new uint8_t[size];
+        maxStreamSize = streamMax;
+        position = buffer;
+        stream = strm;
+        size = dictSize;
+    }
+
+    ~DecoderOutput(){
+        delete[] buffer;
+    }
+};
+
 template<uint32_t numBits, uint32_t size>
 struct ProbTable {
     uint16_t data[size] = { 0 };
@@ -28,8 +79,7 @@ struct ProbTable {
 
 template<uint32_t totalModelBits, uint32_t numMoveBits>
 struct RangeCoder {
-    uint8_t* streamPtr { nullptr };
-    std::size_t streamSize { 0 };
+    uint8_t*& streamPtr { nullptr };
     uint32_t range { 0xFFFFFFFF }, code { 0 };
     uint8_t corrupted { false };
 
@@ -41,7 +91,7 @@ struct RangeCoder {
         }
     }
 
-    uint32_t DecodeDirect(uint8_t* in, uint16_t numBits){
+    uint32_t DecodeDirect(uint16_t numBits){
         uint32_t data { 0 };
 
         do {
@@ -85,7 +135,7 @@ struct RangeCoder {
     }
 
     template<uint32_t bits, uint32_t size>
-    uint16_t DecodeBitTree(uint8_t* in, uint16_t numBits, ProbTable<bits, size>& table){
+    uint16_t DecodeBitTree(uint16_t numBits, ProbTable<bits, size>& table){
         uint16_t m = 1;
         for (uint16_t i = 0; i < numBits; i++) {
             m = (m << 1) + DecodeBit(&table.data[m]);
@@ -94,7 +144,7 @@ struct RangeCoder {
     }
 
     template<uint32_t bits, uint32_t size>
-    uint16_t DecodeBitTreeReverse(uint8_t* in, uint16_t numBits, ProbTable<bits, size>& table){
+    uint16_t DecodeBitTreeReverse(uint16_t numBits, ProbTable<bits, size>& table){
         uint16_t m = 1;
         uint16_t symb = 0;
         for (uint16_t i = 0; i < numBits; i++){
@@ -105,15 +155,16 @@ struct RangeCoder {
         return symb;   
     }
 
-    RangeCoder(uint8_t* stream, std::size_t size){
-        streamSize = size;
+    RangeCoder(uint8_t*& stream){
         streamPtr = stream;
 
         uint8_t b = *streamPtr;
         streamPtr++;
 
-        for(std::size_t i = 0; i < 4; i++)
+        for(std::size_t i = 0; i < 4; i++){
             code = (code << 8) | *streamPtr;
+            streamPtr++;
+        }
 
         if(b != 0 || code == range)
             corrupted = true;
@@ -126,13 +177,34 @@ struct LiteralCoder {
     uint16_t state { 0 };
     ProbTable<11, 0x300>* literalProbabilities { nullptr };
     
-    void DecodeLiteral(uint8_t* outBuffer, std::size_t outSize, uint32_t rep){
+    void DecodeLiteral(uint8_t* in, DecoderOutput& output, uint32_t rep){
         uint16_t prevByte = 0;
     
-        if(outSize != 0){
-            prevByte = *(outBuffer-1); // not sure if this is entirely right
+        if(!output.IsEmpty()){
+            prevByte = output[1];
         }
+
+        uint16_t symbol = 1;
+        uint16_t literalState = (output.runningTotal & ((1 << lp) - 1) << lc) + (prevByte >> (8 - lc));
+
+        ProbTable<11, 0x300> table = literalProbabilities[literalState];
     
+        RangeCoder<11, 5> rangeCoder(in);
+
+        if(state >= 7){
+            uint16_t matchbyte = output[rep0 + 1];
+            do {
+                uint16_t matchBit = (matchbyte >> 7) & 1;
+                uint16_t bit = rangeCoder.DecodeBit(&table.data[((1 + matchBit) << 8) + symbol]);
+                symbol = (symbol << 1 | bit):
+                if(matchBit != bit) break;
+            } while(symbol < 0x100);
+        }
+        
+        while(symbol < 0x100) symbol = (symbol << 1) | rangeCoder.DecodeBit(&table[symbol]);
+
+        output += static_cast<uint8_t>(symbol - 0x100);
+
     }
 
     LiteralCoder(int count){
@@ -166,12 +238,13 @@ bool LZMADecompress(uint8_t* in, std::size_t inSize, uint8_t* out, std::size_t& 
     uint8_t* reader = in;
 
     LZMADecoderInfo decoder;
-    RangeCoder<11, 5> rangeCoder(in, inSize);
-    LiteralCoder literals(decoder.literalPositionBits + decoder.literalContextBits);
-
     // Decode Stream Header
     decoder.InitProps(in);
     reader += 5; // skip past 
+
+    DecoderOutput output(decoder.dictSize, out, outSize);
+    LiteralCoder literals(decoder.literalPositionBits + decoder.literalContextBits, in, inSize);
+
 
     return rangeCoder.corrupted;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 ## BeeCoders
 
-A series of single header libraries for encoding/deocding various formats. Made because while working on other projects I couldn't find and libraries with an api I liked with a permissive license.
+A series of single header libraries for encoding/deocding various formats.
+
+Made because while working on other projects I couldn't find and libraries with an api I liked with a permissive license. Buzz :)
